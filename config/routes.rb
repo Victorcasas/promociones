77f15cccaab35c3d7ec9bf795dfc88b2f19052a6
v <you@example.com>
2013-11-20 15:52:24 +0100

    1.0

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ LearnRails::Application.routes.draw do
   resources :contacts, only: [:new, :create]
   resources :visitors, only: [:new, :create]
   resources :calculos, only: [:new, :create]
-  root to: 'index.html'
+  root to: 'pages#index'
 end
